6661e6afb89edf5ea9aad18665fad86fcacd3dbb
egmartin83 <egmartin83@gmail.com>
2023-03-13 13:03:52 -0300

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/egmartin83/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('egmartin83')
+            node / gitConfigEmail('egmartin83@gmail.com')
         }
     }
     triggers {
