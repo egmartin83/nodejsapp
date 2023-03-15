43c3987388101d6f3413547de66cc73812740864
egmartin83 <egmartin83@gmail.com>
2023-03-14 21:54:25 -0300

    Update nodejsDockerDSL.groovy

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,10 +1,12 @@
 job('Aplicacion Node.js Docker DSL EMITO') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    label('jenkins-agent')
     scm {
         git('https://github.com/egmartin83/nodejsapp.git', 'master') { node ->
             node / gitConfigName('egmartin83')
             node / gitConfigEmail('egmartin83@gmail.com')
         }
+
     }
     wrappers {
         nodejs('nodejs')
@@ -12,7 +14,6 @@ job('Aplicacion Node.js Docker DSL EMITO') {
     steps {
         dockerBuildAndPublish {
             repositoryName('egmartin83/nodejsapp')
-	    labelExpression('jenkins-agent')
 	    dockerHostURI('tcp://192.168.49.2:2376')
 	    serverCredentials('3c1cb828-0a01-48b0-8522-9cde5b1cfb00')
             tag('${GIT_REVISION,length=7}')
